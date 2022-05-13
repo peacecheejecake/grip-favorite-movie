f2564be79888a6f2baad5c40d9b95353dce20235
Min Jiwon <peace.cheejecake@gmail.com>
2022-05-13 17:28:05 +0900

    assets: add svg icons

--- src/assets/svgs/index.js
+++
@@ -1,2 +0,0 @@
-export { ReactComponent as StarIcon } from './star.svg';
-export { ReactComponent as CheckIcon } from './check.svg';
---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as StarIcon } from './star_48x48.svg';
+export { ReactComponent as FilledStarIcon } from './filled_star_48x48.svg';
+export { ReactComponent as SearchIcon } from './search_48x48.svg';
+export { ReactComponent as CheckIcon } from './check_20x20.svg';
