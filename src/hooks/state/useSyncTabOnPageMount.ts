eb23b8dcd74feece926d9ee3885e58cadbce1dd2
Min Jiwon <peace.cheejecake@gmail.com>
2022-05-13 17:29:16 +0900

    feat: useDarkModeState, useRecoil, and useSyncTabOnPageMount hooks

--- src/hooks/state/useSyncTabOnPageMount.ts
+++ src/hooks/state/useSyncTabOnPageMount.ts
@@ -2,7 +2,7 @@ import { useMount } from 'react-use';
 import { useRecoilState } from 'recoil';
 import { selectedTabState } from '@states/selectedTabState';
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
 export const useSyncTabOnPageMount = () => {
   const [tabIdx, setTabIdx] = useRecoilState(selectedTabState);
